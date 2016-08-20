7facfe833af8aa5869e99603f876747d80b56167
占峰 <18272734568@163.com>
2016-08-20 14:41:16 +0800

    add remote git

---
+++ BirthdayProject/AVFoundation/ZFAVAudioManager.h
@@ -0,0 +1,13 @@
+//
+//  ZFAVAudioManager.h
+//  BirthdayProject
+//
+//  Created by Daisy on 16/8/20.
+//  Copyright © 2016年 zf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZFAVAudioManager : NSObject
+
+@end
---
+++ BirthdayProject/AVFoundation/ZFAVAudioManager.m
@@ -0,0 +1,13 @@
+//
+//  ZFAVAudioManager.m
+//  BirthdayProject
+//
+//  Created by Daisy on 16/8/20.
+//  Copyright © 2016年 zf. All rights reserved.
+//
+
+#import "ZFAVAudioManager.h"
+#import <AVFoundation/AVFoundation.h>
+@implementation ZFAVAudioManager
+
+@end
